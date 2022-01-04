9ecec4212b34115ca98d274f29aaf8e88c9889dd
Chris Insignares <insignaresc115@gmail.com>
2022-01-03 23:04:33 -0500

    update for push to ghpages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,12 +6,11 @@ export default function About() {
       <h1>About Page</h1>
       <p>
         I’m a multi-media artist with a background in many audio-visual and
-        technical fields. In searching to learn the many ways the world
-        communicates to each other, I’ve found myself producing/broadcasting
-        local television in the public sector, a contractor for architectural
+        technical fields. I’ve found myself producing/broadcasting local
+        television in the public sector, a contractor for architectural
         photography, a gigging musician around NYC, an editor/producer of video
-        content in the commercial field, and also developing a burgeoning career
-        in tech and code-based languages.
+        content in the commercial field, and I am now developing a burgeoning
+        career in computer programming.
       </p>
     </div>
   );
